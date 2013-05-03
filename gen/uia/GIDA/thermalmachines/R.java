84e0a09d6c4b43d944391fbf62cac988892d1ddc
Miguel Kelly <m26.kelly@gmail.com>
2013-05-02 22:00:11 -0500

    corrected some mistakes

--- gen/uia/GIDA/thermalmachines/R.java
+++ gen/uia/GIDA/thermalmachines/R.java
@@ -7,8 +7,6 @@
 
 package uia.GIDA.thermalmachines;
 
-import android.view.View;
-
 public final class R {
     public static final class attr {
     }
@@ -26,40 +24,53 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int fondoibero=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int backgrounds_125=0x7f020000;
+        public static final int fondoibero=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int CalcuButton=0x7f09000c;
-        public static final int EditBore=0x7f090002;
-        public static final int EditN=0x7f090008;
-        public static final int EditRc=0x7f090007;
-        public static final int EditStroke=0x7f090006;
-        public static final int action_settings=0x7f090010;
-        public static final int editText1=0x7f09000e;
-        public static final int editText2=0x7f09000f;
-        public static final int textView1=0x7f090000;
-        public static final int textView2=0x7f090001;
-        public static final int textView3=0x7f090003;
-        public static final int textView4=0x7f090004;
-        public static final int textView5=0x7f090005;
-        public static final int textView6=0x7f09000a;
-        public static final int textView7=0x7f09000b;
-        public static final int textView8=0x7f09000d;
-        public static final int view1=0x7f090009;
+        public static final int aboutButton=0x7f09000c;
+        public static final int action_settings=0x7f09000e;
+        public static final int bore=0x7f090001;
+        public static final int calculateButton=0x7f09000d;
+        public static final int n=0x7f090007;
+        public static final int pa=0x7f090009;
+        public static final int rc=0x7f090005;
+        public static final int stroke=0x7f090003;
+        public static final int ta=0x7f09000b;
+        public static final int tableRow1=0x7f090000;
+        public static final int tableRow2=0x7f090002;
+        public static final int tableRow3=0x7f090004;
+        public static final int tableRow4=0x7f090006;
+        public static final int tableRow5=0x7f090008;
+        public static final int tableRow6=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_main_activity_otto=0x7f030000;
-		public static View activity_results2;
+        public static final int activity_results2=0x7f030001;
     }
     public static final class menu {
         public static final int main_activity_otto=0x7f080000;
-		public static int results2;
+        public static final int results=0x7f080001;
+        public static final int results2=0x7f080002;
     }
     public static final class string {
+        public static final int About=0x7f05000b;
+        public static final int Air=0x7f050008;
+        public static final int Bore=0x7f050004;
+        public static final int Calculate=0x7f05000d;
+        public static final int N=0x7f050007;
+        public static final int OttoName=0x7f05000e;
+        public static final int Pa=0x7f050009;
+        public static final int Rc=0x7f050006;
+        public static final int Result=0x7f05000c;
+        public static final int Stroke=0x7f050005;
+        public static final int Ta=0x7f05000a;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_results=0x7f050003;
+        public static final int title_activity_results2=0x7f05000f;
     }
     public static final class style {
         /** 
